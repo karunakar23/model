f38d8515071f3051494bee3cb3a12a51d91e2bcf
karunakar23 <71214328+karunakar23@users.noreply.github.com>
2022-03-18 16:16:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 model = pickle.load(open('car_price_predictor.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
-    return render_template('index.html')
+    return render_template('home.html')
 standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
 def predict():
@@ -41,11 +41,11 @@ def predict():
         prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
         output=round(prediction[0],2)
         if output<0:
-            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+            return render_template('output.html',prediction_text="Sorry you cannot sell this car")
         else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+            return render_template('output.html',prediction_text="You Can Sell The Car at {}".format(output))
     else:
-        return render_template('index.html')
+        return render_template('home.html')
 
 if __name__=="__main__":
     app.run(debug=True)
